71f9d36b428098899f9f61e168238056de63c34a
PeiYumeng <457492875@qq.com>
2020-04-08 09:11:38 +0800

    modify

--- Menubar/menubar.js
+++ Menubar/menubar.js
@@ -2,67 +2,98 @@ function Menu(){
     var html =''
                 +'<div class="menu-box">'
                 +'    <ul class="menu-name">'
-                +'       <li class="menu-title">文件(F)</li>'
-                +'  </ul>'
-                +'  <ul class="menu-context">'
-                +'      <li class="menu-item">'
-                +'          <span class="menu-item-left">新建(N)</span>'
-                +'          <span class="menu-item-right">Ctrl+N</span>'
-                +'      </li>'
-                +'      <hr>'
                 +'  </ul>'
                 +'</div>',
             cfg;
-    this.styles='';
+    var index = -1;
+    var active = false;
+    var active_num = 0;
+    var s = -1;
+    var hr = ['',''];
     this.show = function show(conf){
         cfg = {
             container:'body',
-            boxs:[],
-            title:'',
-            $div:'',
-            $input:$('<input />'),
-            $list:$('<li></li>'),
-            $listBox:$('<box></box>'),
         };
         $.extend(cfg,conf);
         var $dlg = $(html);
-        cfg.$list = $dlg.find('.list-ul');
-        cfg.$listBox = $dlg.find('.list-title');
-        cfg.$input = $dlg.find('.list-input');
-        cfg.$div = $dlg.find('.list')
         $(cfg.container).append($dlg);
-        addtitle();
-        addli();
+        adddata();
+        console.log(hr)
     }
-    function addtitle(){
-        cfg.$listBox.append(cfg.title)
-    }
-    function style(i){
-        switch(cfg.title){
-            case '字体':
-                cfg.$div.addClass('list1')
-                $item.css({'font-family': cfg.boxs[i]});
-                break
-            case '字号':
-                cfg.$div.addClass('list3')
-                $item.css({'font-size': cfg.boxs[i]});
-                break;
-            case '样式':
-                cfg.$div.addClass('list2')
-                if(cfg.boxs[i] === '斜体') {$item.css({'font-style': 'italic'})}
-                if(cfg.boxs[i] === '粗体') {$item.css({'font-weight': 'bold'})}
-                if(cfg.boxs[i] === '粗偏斜体') {$item.css({'font-weight': 'bold', 'font-style': 'italic'})}
-                break; 
-        }
-    }
-    function addli(){
-        for(var i=0;i<cfg.boxs.length;i++){
-            $item = $('<li class="list-li"></li>');
-            style(i)
-            cfg.$list.append($item.html(cfg.boxs[i]));
+    function adddata(){
+        var len = menuData.length;
+        for(var i=0;i<len;i++){
+            var $liT = $('<li class="menu-title"></li>');
+            $('.menu-name').append($liT.html(menuData[i].title));
+            var $ul = $('<ul class="menu-context"></ul>');
+            $ul.css('left',70*i+'px')
+            $('.menu-box').append($ul)
+            for(var a=0;a<menuData[i].menuItems.length;a++){
+                if(menuData[i].menuItems[a].title =='hr'){
+                    var $liC = $('<li class="menu-item"></li>');
+                    var $hr = $('<hr/>')
+                    $ul.append($liC)
+                    $liC.append($hr)
+                    hr[i] += a+',';
+                }else{
+                    var $liC = $('<li class="menu-item"></li>');
+                    var $sp1 = $('<span class="menu-item-left"></span>')
+                    var $sp2 = $('<span class="menu-item-right"></span>')
+                    var $br = $('<br/>')
+                    $liC.attr('attr',i)
+                    $ul.append($liC)
+                    $liC.append($sp1.html(menuData[i].menuItems[a].title))
+                    $liC.append($sp2.html(menuData[i].menuItems[a].shortcut));
+                    // $ul.append($br);
+                    (function(a){
+                        $liC.mouseover(()=>{showSpan(a)})
+                    }(a))
+                }
+            }
             (function(i){
-                $item.click(()=>{selectLi(i)})
+                $liT.click(()=>{titleClick(i)})
+                $ul.mouseleave(()=>{titleout(i)})
+                $ul.mouseover(()=>{ulOver(i)})
+                $liT.mouseover(()=>{titleStyle(i)})
+                $liT.mouseout(()=>{
+                    $('.menu-title').css('background','white')
+                })
             }(i))
         }
+    }
+    function titleStyle(i){//滑过菜单
+        index = i;
+        $('.menu-title').css('background','white')
+        $('.menu-title').eq(i).css('background','#87CEFA')
+        if(active){
+            $('.menu-context').css('display','none')
+            $('.menu-context').eq(i).css('display','block')
+        }
+    }
+    function titleClick(i){//点击每项
+        active_num++;
+        if(active_num!=2){
+            active = true;
+            index = i;
+            $('.menu-context').eq(i).css('display','block')
+            $('.menu-title').eq(i).css('background','#87CEFA')
+        }else{//点了俩下关闭子菜单
+            active_num = 0;
+            active = false;
+            index = -1;
+            $('.menu-context').css('display','none')
+        }
+    }
+    function ulOver(i){//划过子菜单
+        $('.menu-title').eq(i).css('background','#87CEFA')
+    }
+    function titleout(i){//划出子菜单
+        $('.menu-title').css('background','white')
+        $('.menu-context').eq(i).css('display','none')
+    }
+    function showSpan(a){
+        // var aa = $('.menu-context').eq(index).children().eq(a);
+        // console.log( $('.menu-context').eq(index).children())
+        console.log($('.1'))
     }
 }
