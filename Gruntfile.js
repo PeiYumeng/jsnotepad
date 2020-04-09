2ea208cde3884b4c5d48503639e31dc207ba9130
PeiYumeng <457492875@qq.com>
2020-04-09 12:15:14 +0800

    add

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+    //静态代码检查
+      eslint: {
+        options: {
+            configFile: '.eslintrc.json'
+        },
+          target: ['font/font.js']
+      },
+      csslint: {
+        options: {
+            csslintrc: '.csslintrc'        
+        },
+          src: 'font/font.css'
+      },
+      htmlhint: {
+        options: {
+            htmlhintrc: '.htmlhintrc'    
+        },
+        src: '*.html'          
+      },
+    //压缩
+    htmlmin: {
+      options: {
+          collapseWhitespace: true,
+          preserveLineBreaks: false                      
+      },
+      files: {
+          src: './index.html',
+          dest: 'dist/index.html'                            
+      }    
+    },
+    cssmin: {
+          'dist/font/list.css': './font/list.css',
+          'dist/font/font.css': './font/font.css',
+          'dist/Menubar/menubar.css': './Menubar/menubar.css',
+    },
+    uglify: {
+      release:{
+        files: {
+          'dist/font/list.js': './font/list.js',
+          'dist/font/font.js': './font/font.js',
+          'dist/Menubar/menubar.js': './Menubar/menubar.js',  
+          'dist/Menubar/menu-data.js': './Menubar/menu-data.js',  
+        }
+      }         
+    }
+    });
+  
+      grunt.loadNpmTasks('grunt-contrib-csslint');
+      grunt.loadNpmTasks('grunt-htmlhint');
+      grunt.loadNpmTasks('grunt-eslint');
+      grunt.loadNpmTasks('grunt-contrib-htmlmin');
+      grunt.loadNpmTasks('grunt-contrib-cssmin');
+      grunt.loadNpmTasks('grunt-contrib-uglify');
+      grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+      grunt.registerTask('min', ['cssmin','htmlmin','uglify']);
+      grunt.registerTask('default', ['htmlhint', 'csslint', 'eslint','uglify','cssmin','htmlmin']);
+  };
---
+++ dist/font/font.js
@@ -0,0 +1,48 @@
+var $dlgFont = (function(){
+    var html = ''
+               +' <div class="font">'
+               +' <div class="titlebar">'
+               +'     <span class="about-title">字体</span>'
+               +'     <span class="about-close" title="关闭">✖</span>'
+               +' </div>'
+               +' <div class="font-main">'
+               +'     <div class="font-box"></div>'
+               +'    <fieldset class="search-direction">'
+               +'         <legend>示例</legend>'
+               +'               <span class="shili">AaBbYyZz</span>'
+               +'     </fieldset>'
+               +'    <span class="font-foot">脚本(R):</span>'
+               +'     <br>'
+               +'     <select class="font-select">'
+               +'        <option value="">西欧语言</option>'
+               +'        <option value="">中文语言</option>'
+               +'    </select>'
+               +'    <input class="btn-ok" type="button" value="确定">'
+               +'     <input class="btn-cancel" type="button" value="取消">'
+               +'   </div>'
+               +'   </div>'
+            $dlg = $(html),
+            cfg = {
+                container:'body',
+            };
+    function show(conf){
+        $(cfg.container).append($dlg);
+        $.extend(cfg,conf);
+
+        $('.about-close').click(()=>{
+            $('.list-box').remove();
+            $dlg.remove();
+        })
+        $('.btn-cancel').click(()=>{
+            $('.list-box').remove();
+            $dlg.remove();
+        })
+        $('.btn-ok').click(()=>{
+            $('.list-box').remove();
+            $dlg.remove();
+        })
+    }
+    return {
+        show:show
+    }
+}())
